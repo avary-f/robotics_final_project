544cf02fae5d9a7394a4dde7e1360f7f2504208e
Avary Fielding <avaryef@byu.edu>
2025-11-07 15:29:58 -0700

    camera_calibration, adding code for publishing checkerboard

---
+++ camera_calibration.py
@@ -0,0 +1,26 @@
+image_width: 1280
+image_height: 800
+camera_name: narrow_stereo
+camera_matrix: 
+    rows: 3
+    cols: 3
+    data: [415.12614, 0., 665.52373,
+           0., 410.49986, 460.14929,
+           0., 0., 1.               ]
+distortion_model: plumb_bob
+distortion_coefficents: 
+    rows: 1
+    cols: 5
+    data: [-0.017697, -0.007270, 0.005751, -0.000139, 0.]
+rectification_matrix: 
+    rows: 3
+    cols: 3
+    data: [1., 0., 0.,
+           0., 1., 0.,
+           0., 0., 1.]
+projection_matrix:
+    rows: 3
+    cols: 4
+    data: [415.12614, 0., 665.52373, 0.,
+           0., 410.27547, 459.89776, 0.
+           0., 0., 1., 0.]
